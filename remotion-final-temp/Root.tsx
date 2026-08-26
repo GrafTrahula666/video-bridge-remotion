6e80ad85e26f67e69b58edc587ac4eac97469917
GrafTrahula666 <origtopg777@gmail.com>
2026-08-27 02:22:50 +0300

    Make Remotion duration follow detected overlap

--- remotion-final-temp/Root.tsx
+++ remotion-final-temp/Root.tsx
@@ -2,5 +2,19 @@ import {Composition} from 'remotion';
 import {RitualComposition} from './Composition';
 
 export const RemotionRoot = () => (
-  <Composition id="RitualFinal" component={RitualComposition} durationInFrames={721} fps={24} width={828} height={1108} defaultProps={{firstFrames:361, secondFrames:361, overlapFrames:1}} />
+  <Composition
+    id="RitualFinal"
+    component={RitualComposition}
+    durationInFrames={721}
+    fps={24}
+    width={828}
+    height={1108}
+    defaultProps={{firstFrames:361, secondFrames:361, overlapFrames:1}}
+    calculateMetadata={({props}) => ({
+      durationInFrames: props.firstFrames + Math.max(1, props.secondFrames - props.overlapFrames),
+      fps: 24,
+      width: 828,
+      height: 1108,
+    })}
+  />
 );
